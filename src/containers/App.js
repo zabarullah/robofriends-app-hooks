b9d0117f234b497c82489279e819a336152c0e23
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-01-01 14:15:44 +0000

    first commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import './App.css';
 import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
+function App() {
 
-    componentDidMount() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=> response.json())
-            .then(users => {this.setState({ robots: users })});
+            .then(response => response.json())
+            .then(users => { setRobots(users) });
+            console.log(count); // Count button (below) was created to test how useEffect works with dependency, preventing loops and re-rendering
+    },[count]) // count here is the dependency whereby if count changes then useEffect will run and render again each time
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
 
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        if (robots.length === 0) {
-            return <h1> loading </h1>
-        } else {
+    if (robots.length === 0) {
+        return <h1> loading </h1>
+    } else {
         return (
             <div className='tc'>
-                <h1 className= 'f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <h1 className='f1'>RoboFriends</h1>
+                <button onClick={() => setCount(count+1)}> Click Me to see the count increase in the console</button>
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundry>
-                        <CardList robots={filteredRobots}/>
+                        <CardList robots={filteredRobots} />
                     </ErrorBoundry>
                 </Scroll>
-            </div>    
+            </div>
         );
-        }
     }
-
 }
 
 export default App;
